c66c7982cad8e7b0f6ec66b45e22a30fb1877e0a
unknown <jacky.zhao2019@stgeorges.bc.ca>
2018-09-26 10:16:22 -0700

    Working Drone Plot, Drone Class, and Random Init

---
+++ MapDisp.py
@@ -0,0 +1,41 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product, combinations
+from drone import *
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+# draw sphere
+r = 1
+pi = np.pi
+phi, theta = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j] # phi = alti, theta = azi
+x = r*np.sin(phi)*np.cos(theta)
+y = r*np.sin(phi)*np.sin(theta)
+z = r*np.cos(phi)
+z = z.clip(min=0)
+ax.plot_wireframe(x, y, z, color="k", alpha=0.1)
+ax.set_xlim3d(-1, 1)
+ax.set_ylim3d(-1,1)
+ax.set_zlim3d(0,1)
+
+def initDrone(n):
+    dr = []
+    for i in range(n):
+        x,y,x_vel,y_vel,z_vel = 2*np.random.random((5,))-1
+        z = np.random.random()
+        dr.append(drone(x,y,z,x_vel,y_vel,z_vel))
+    return dr
+
+d_arr = initDrone(20)
+X = []
+Y = []
+Z = []
+for d in d_arr:
+    X.append(d.g_x)
+    Y.append(d.g_y)
+    Z.append(d.g_z)
+
+ax.scatter(X,Y,Z,s=10,c="r")
+plt.show()
+print(d_arr[0].getDistDet(d_arr[1]))
---
+++ drone.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class drone():
+    #Global Position Vars
+    g_x = 0
+    g_y = 0
+    g_z = 0
+
+    #Global Velocity Vars
+    xv = 0
+    yv = 0
+    zv = 0
+
+    def __init__(self,x,y,z,x_vel,y_vel,z_vel):
+        self.g_x = x
+        self.g_y = y
+        self.g_z = z
+        self.xv = x_vel
+        self.yv = y_vel
+        self.zv = z_vel
+
+    def getPos(self):
+        return self.g_x, self.g_y, self.g_z, self.xv, self.yv, self.zv
+
+    def __str__(self):
+        return "x: " + str(self.g_x) + " y: " + str(self.g_y) + " z: " + str(self.g_z) + " x_vel: " + str(self.xv) + " y_vel: " + str(self.yv) + " z_vel: " + str(self.zv)
+
+    def getDistDet(self,drone):
+        selfpos = self.getPos()
+        dronepos = drone.getPos()
+        raw = np.subtract(dronepos,selfpos)
+        r = np.sqrt(np.square(raw[0])+np.square(raw[1])+np.square(raw[2])) #r = sqrt(x^2+y+^2+z^2)
+        theta = np.arccos(raw[3]/r)   #theta =arccos(z/r))
+        phi = np.arctan(raw[1]/raw[0])   #phi = arctan(y/x).
+        #THETA is Elevation
+        #PHI is Azimuth
+        return r, phi, theta

8d3894b2e4c2ec8f53becd61667af374cadb35df
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-04-13 15:31:50 -0700

    Updated piscript.sh

--- LOCAL_Hardware/recogNet/picScript.sh
+++ LOCAL_Hardware/recogNet/picScript.sh
@@ -1,8 +1,11 @@
 lockloc="locks/IM_LOCK"
-while [ ! -f "$lockloc" ]; do
-    echo 'Lock not found, waiting 1 second'
-    sleep 1
-done
+while ;; do
+  while [ ! -f "$lockloc" ]; do
+      echo 'Lock not found, waiting 1 second'
+      sleep 1
+  done
 
-echo 'Lock found! Taking picture...'
-raspistill -o "imgs/tmp.jpg"
+  echo 'Lock found! Taking picture...'
+  raspistill -o "imgs/tmp.jpg"
+  rm /home/pi/recogNet/locks/IM_LOCK
+done
